74b2fe4e812782a357f5dd50de7b669f4caca1fa
tamilmani-p29 <129751687+tamilmani-p29@users.noreply.github.com>
2024-05-28 15:47:48 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const app = express();
+
+console.log(process.env.PASSWORD)
+const conn = mysql.createConnection({
+    user: process.env.USER,
+    host: process.env.HOST,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+})
+
+conn.connect((err) => {
+    if(err) throw err;
+    console.log("Connected to DB")
+})
+
+app.use(cors());
+app.use(express.static(__dirname + '/static'));
+
+app.get("/todos", async (req, res) => {
+  console.log("inside get users");
+  conn.query("SELECT * FROM todostable.todo", (err, result) => {
+    if(err) {
+        res.status(422).json("No data available")
+    } else {
+        res.status(201).json(result)
+    }
+  })
+});
+
+app.get("/", async (req, res) => {
+  res.sendFile("static/index.html", {root: __dirname})
+})
+
+app.listen(8000, () => {
+  console.log("server listening in port 8000");
+});
